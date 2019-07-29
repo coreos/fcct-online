8fb674304fa9048dea8b8378a5daa9e12363146c
Allen Bai <abai@redhat.com>
2019-07-29 10:24:19 -0400

    server: Add setup script to downlaod and initialize fcct binary

---
+++ server/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+ver='v0.1.0'
+
+rm -f ./fcct-*
+
+# Download fcct binary and detached signature
+curl -O -L "https://github.com/coreos/fcct/releases/download/${ver}/fcct-x86_64-unknown-linux-gnu"
+curl -O -L "https://github.com/coreos/fcct/releases/download/${ver}/fcct-x86_64-unknown-linux-gnu.asc"
+
+# Download Coreos Application Signing Key and import it
+curl -O -L http://coreos.com/dist/pubkeys/app-signing-pubkey.gpg
+gpg --import app-signing-pubkey.gpg
+
+# Verify the downloaded binary
+gpg --verify fcct-x86_64-unknown-linux-gnu.asc fcct-x86_64-unknown-linux-gnu
+
+chmod +x fcct-x86_64-unknown-linux-gnu
+
+echo "Finish setup"
