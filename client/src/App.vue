b403cff6bddcf02974fdfce771d6687002faf0e6
Allen Bai <abai@redhat.com>
2019-07-31 14:20:23 -0400

    Nav.vue: Add navbar and update icons

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
@@ -16,7 +16,7 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+/* #nav {
   padding: 30px;
 }
 
@@ -27,5 +27,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
---
+++ client/src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <!-- eslint-disable-next-line -->
+        <img src="../assets/logo-inline-coreos.png" width="118" height="24" class="d-inline-block align-top" alt="">
+    </a>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
