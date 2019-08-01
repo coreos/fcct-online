caa8963683d0b0b3c9d6762c06ba8ad2a7499bae
Allen Bai <abai@redhat.com>
2019-08-01 14:18:20 -0400

    app.py: cleanup test code

--- server/app.py
+++ server/app.py
@@ -23,10 +23,7 @@ def index():
 # Since we used path: here, the fcc cannot contain \n
 @app.route('/config/<path:fcc>', methods=['GET', 'POST'])
 def fcc_to_ignition(fcc):
-    # test data
-    encoded_string = 'variant%3A%20fcos%5C%6Eversion%3A%201.0.0%5C%6Epasswd%3A%5C%6E%20%20users%3A%5C%6E%20%20%20%20-%20name%3A%20core%5C%6E%20%20%20%20%20%20ssh_authorized_keys%3A%5C%6E%20%20%20%20%20%20%20%20-%20ssh-rsa%20AAAAB3NzaC1yc2EAAAADAQABAAABgQC6jeR8fMqwxDsoVdc62m%2BV6L9QWH7MSJ%2BzmmrE0UWClF3iuFJ5belHxXNg%2F6wnpefyu5hKrrwCGjcfxNxH438MYuUOBxo5CwZHI3d3%2BkkMudidEhFJxMhk%2BS9I5XnBRjhzQrTnqcf2BULW4kV6JfrLSx2TQxvaUh9d59tJa5J6sFtob6Iil%2FqY9oyKQQHA%2F%2B7%2BxgCqWW2P%2FlgkBCLk1%2Bll%2BF0nNCTygUJjWyRWy3lUDo5edwyV8cqoYJVmE1dcBAqV7GwflrlSf%2BRzAVa8S5hxpdKyXs9SVVlFoN3%2FSjwaJnJX%2FoQWPbubraBm3%2F7sAC2pLAo9BuSRYdylUJyS9NGhNK2ibdB4o5rWREynnX6Lr4FotzIAQg%2BxDFRc9EX%2BsHHaTOWXDebFzL4aJEPn1XA0hQ5zzK5PZyL6l1v%2Bqiz16o4hO4PlRaW5FmNndjCJ3yREalIZBp6a9PCdNw9K4LPcPQ1ZYjxx6bn1SWXbRodKpdSQqLlzKeUNjH0Aro6%2BTB8%3D%20abai%40unused-10-15-17-19.yyz.redhat.com'
     decoded_string = parse.unquote(fcc).replace("\\n", "\n")
-
     response_object = {}
 
     # create tmp file to store config
