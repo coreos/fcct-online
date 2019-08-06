17f3f699f35705c366ab3026e8fddcfe3d782437
Allen Bai <abai@redhat.com>
2019-08-06 16:16:29 -0400

    server/app.py: Use POST instead of GET
    Since URL cannot contain \n, we would have to pass \\n in URL and
    substitue \\n to \n from the backend. Using POST would allow passing
    \n as string directly inside POST body. Pass config as URL is not
    necessary since front end will handle the URL parsing and pass the
    parsed string to back end.

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ import uuid
 from subprocess import Popen, PIPE, STDOUT
 import urllib.parse as parse
 
-from flask import Flask, jsonify, redirect, url_for
+from flask import Flask, jsonify, redirect, url_for, request
 from flask_cors import CORS
 
 
@@ -21,24 +21,24 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 # Since we used path: here, the URL encoding cannot contain \n
 # So use \\n instead and substitute to \n during parsing
 @app.route('/', defaults={'fcc': ''})
-@app.route('/config/')
-@app.route('/config/<path:fcc>', methods=['GET'])
+@app.route('/config/', methods=['GET','POST'])
 def fcc_to_ignition(fcc=''):
-    decoded_string = parse.unquote(fcc).replace("\\n", "\n")
-    response_object = {}
+    if (request.method == 'POST'):
+        post_data = request.get_json()
+        response_object = {}
 
-    # handle '/config/'
-    if decoded_string == '':
+        # handle '/config/'
+        if post_data == '':
+            return jsonify(response_object)
+
+        # run fcct on the config
+        ignition_config = Popen(
+            './fcct-x86_64-unknown-linux-gnu',
+            stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+        stdout = ignition_config.communicate(input=post_data.get('config_string').encode())[0]
+
+        response_object['ignition_config'] = stdout.decode()
         return jsonify(response_object)
 
-    # run fcct on the config
-    ignition_config = Popen(
-        './fcct-x86_64-unknown-linux-gnu',
-        stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-    stdout = ignition_config.communicate(input=decoded_string.encode())[0]
-
-    response_object['ignition_config'] = stdout.decode()
-    return jsonify(response_object)
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)
