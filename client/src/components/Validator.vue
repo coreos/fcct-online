421ef532521b1a954ee0a5a2ebeb7b3b040b855e
Allen Bai <abai@redhat.com>
2019-08-06 16:20:20 -0400

    Validator.vue: Add button to generate encoded URL
    Stops passing URL directly to backend, instead parse the URL
    to decoded string and send a POST request to backend. This way,
    no replacement between '\\n' and '\n' is needed, which was a bad
    idea in the first place.

--- client/src/components/Validator.vue
+++ client/src/components/Validator.vue
@@ -18,6 +18,10 @@
     </div>
     <!-- eslint-disable-next-line -->
     <input type="submit" id="validate-submit" class="btn btn-primary" value="Transpile" data-category="Validator" v-on:click="submit">
+    <!-- eslint-disable-next-line -->
+    <button type="button" id="generate-url" class="btn btn-secondary" v-on:click="showURL">Generate URL From FCC</button>
+    <!-- eslint-disable-next-line -->
+    <textarea readonly v-if="visible" v-model="encoded_url" id="encoded-url-box" wrap="off" spellcheck="false" autofocus="" rows="3"></textarea>
   </form>
 </div>
 </template>
@@ -25,26 +29,34 @@
 <script>
 import axios from 'axios';
 
+const serverURL = '/config/';
+const clientURL = window.location.origin.concat('/');
+
 export default {
   name: 'Validator',
+
   data() {
     return {
       fcc_config: '',
       ignition_config: '',
+      encoded_url: '',
+      visible: false,
     };
   },
+
   methods: {
     cleanIgnitionBox() {
       this.ignition_config = '';
     },
+
     convertFccToUrl() {
-      const replacedString = this.fcc_config.replace(/\n/g, '\\n');
-      return encodeURI(replacedString);
+      return clientURL.concat(encodeURIComponent(this.fcc_config));
     },
+
     toIgnitionConfig() {
-      const urlConfig = this.convertFccToUrl();
-      const path = '/config/'.concat(urlConfig);
-      axios.get(path)
+      this.encoded_url = this.convertFccToUrl();
+      const postData = { config_string: this.fcc_config };
+      axios.post(serverURL, postData)
         .then((res) => {
           try {
             this.ignition_config = JSON.parse(res.data.ignition_config.ignition_config);
@@ -57,15 +69,25 @@ export default {
           console.error(error);
         });
     },
+
     submit(e) {
       e.preventDefault();
       this.cleanIgnitionBox();
       this.toIgnitionConfig();
     },
+
+    showURL() {
+      // eslint-disable-next-line
+      this.visible = this.fcc_config ? true : false;
+      this.encoded_url = this.convertFccToUrl();
+    },
   },
+
   created() {
     if (this.$route.params.fcc_config) {
       this.fcc_config = this.$route.params.fcc_config;
+      this.encoded_url = this.convertFccToUrl();
+      this.showURL();
       this.toIgnitionConfig();
     }
   },
@@ -98,8 +120,25 @@ export default {
 }
 #validate-submit {
   margin-top: 0.5em;
+  margin-bottom: 1em;
 }
 #container {
   overflow: auto;
+}
+#generate-url {
+  margin-top: 0.5em;
+  margin-left: 0.9em;
+  margin-bottom: 1em;
+}
+#encoded-url-box {
+  width: 95vw;
+  position: relative;
+  background: transparent;
+  border: 0px;
+  font-size: 10pt;
+  font-family: monospace;
+  line-height: 14px !important;
+  box-shadow: 0 0 3px rgba(153,153,153,.75);
+  resize: none;
 }
 </style>
