df3374d681f92ad7d21aec5383775d8847f80097
Allen Bai <abai@redhat.com>
2019-08-06 10:45:12 -0400

    server/app.py: Delete tmpfile completely and use string input instead server/tmp: Remove tmp directory

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ import uuid
 from subprocess import Popen, PIPE, STDOUT
 import urllib.parse as parse
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, url_for
 from flask_cors import CORS
 
 
@@ -18,38 +18,27 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify('index')
-
-
-# Since we used path: here, the fcc cannot contain \n
-@app.route('/config/<path:fcc>', methods=['GET', 'POST'])
-def fcc_to_ignition(fcc):
+# Since we used path: here, the URL encoding cannot contain \n
+# So use \\n instead and substitute to \n during parsing
+@app.route('/', defaults={'fcc': ''})
+@app.route('/config/')
+@app.route('/config/<path:fcc>', methods=['GET'])
+def fcc_to_ignition(fcc=''):
     decoded_string = parse.unquote(fcc).replace("\\n", "\n")
     response_object = {}
 
-    # create tmp file to store config
-    tmp_file_name = uuid.uuid4().hex
-    tmp_file = open("tmp/{}".format(tmp_file_name), "w")
-    tmp_file.write(decoded_string)
-    tmp_file.close()
+    # handle '/config/'
+    if decoded_string == '':
+        return jsonify(response_object)
 
     # run fcct on the config
-    with open("tmp/{}".format(tmp_file_name), "r") as tmp:
-        config = tmp.read()
-        ignition_config = Popen(
-            './fcct-x86_64-unknown-linux-gnu',
-            stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        stdout = ignition_config.communicate(input=config.encode())[0]
-        tmp.close()
+    ignition_config = Popen(
+        './fcct-x86_64-unknown-linux-gnu',
+        stdin=PIPE, stdout=PIPE, stderr=STDOUT)
+    stdout = ignition_config.communicate(input=decoded_string.encode())[0]
 
-    # clean up
-    os.remove("tmp/{}".format(tmp_file_name))
     response_object['ignition_config'] = stdout.decode()
     return jsonify(response_object)
-
 
 if __name__ == '__main__':
     app.run()
