38229af6ed4246fc4fa8ed7c8af52f249019ef69
Allen Bai <abai@redhat.com>
2019-08-08 16:59:09 -0400

    Dockerfile: Try to fix 502 bad gateway on pulled image from quay.io

--- Dockerfile
+++ Dockerfile
@@ -17,13 +17,15 @@ RUN apk update && apk add --no-cache python3 && \
     if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
     if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
     rm -r /root/.cache
-RUN apk update && apk add postgresql-dev gcc python3-dev musl-dev
+RUN apk update && apk add --no-cache gnupg curl gcc python3-dev musl-dev
 COPY --from=build-vue /app/dist /usr/share/nginx/html
 COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
 COPY ./server/requirements.txt ./
+COPY ./setup.sh ./
+RUN ./setup.sh --container
 RUN pip install -r requirements.txt
 RUN pip install gunicorn
 COPY ./server .
-CMD gunicorn -b 0.0.0.0:5000 app:app --daemon && \
+CMD gunicorn -b 127.0.0.1:5000 app:app --daemon --log-file logfile && \
       sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && \
       nginx -g 'daemon off;'
--- setup.sh
+++ setup.sh
@@ -1,22 +1,60 @@
-#!/bin/bash
-set -x
+set -eo pipefail
 
 ver='v0.1.0'
-cd server
-rm -f ./fcct-*
 
-# Download fcct binary and detached signature
-curl -O -L "https://github.com/coreos/fcct/releases/download/${ver}/fcct-x86_64-unknown-linux-gnu"
-curl -O -L "https://github.com/coreos/fcct/releases/download/${ver}/fcct-x86_64-unknown-linux-gnu.asc"
+print_help(){
+    printf "options:\n\
+            --local: setupt for local build\n\
+            --container: setup for container build\n"
+    exit 1
+}
 
-# Download Coreos Application Signing Key and import it
-curl -O -L http://coreos.com/dist/pubkeys/app-signing-pubkey.gpg
-gpg --import app-signing-pubkey.gpg
+setup() {
+    set -x
+    rm -f ./fcct-* app-signing-pubkey.gpg
 
-# Verify the downloaded binary
-gpg --verify fcct-x86_64-unknown-linux-gnu.asc fcct-x86_64-unknown-linux-gnu
+    # Download fcct binary and detached signature
+    curl -O -L "https://github.com/coreos/fcct/releases/download/${ver}/fcct-x86_64-unknown-linux-gnu"
+    curl -O -L "https://github.com/coreos/fcct/releases/download/${ver}/fcct-x86_64-unknown-linux-gnu.asc"
 
-chmod +x fcct-x86_64-unknown-linux-gnu
-cd ..
+    # Download Coreos Application Signing Key and import it
+    curl -O -L http://coreos.com/dist/pubkeys/app-signing-pubkey.gpg
+    gpg --import app-signing-pubkey.gpg
+
+    # Verify the downloaded binary
+    gpg --verify fcct-x86_64-unknown-linux-gnu.asc fcct-x86_64-unknown-linux-gnu
+
+    chmod +x fcct-x86_64-unknown-linux-gnu
+    set +x
+}
+
+if [[ "$#" -eq 0 ]]; then
+    print_help
+fi
+
+# Call getopt to validate the provided input.
+rc=0
+options=$(getopt --options=lc --longoptions=local,container -- "$@") || rc=$?
+[ $rc -eq 0 ] || {
+    print_help
+    exit 1
+}
+eval set -- "$options"
+while true; do
+    case "$1" in
+        -l | --local)
+            cd ./server
+            setup
+            cd ..
+            ;;
+        -c | --container)
+            setup
+            ;;
+        *)
+            break
+            ;;
+    esac
+    shift
+done
 
 echo "Finish setup"
